4ac4316668f7ac2b8df0fb4debe09b1e14d68d0a
nabin <nabinrai55@hotmail.com>
2014-04-07 04:40:23 +0100

    fixed bugs and added .gitignore file

--- app/controllers/peep.rb
+++ app/controllers/peep.rb
@@ -1,6 +1,10 @@
-get '/peeps' do
+post '/peeps' do
 	peep = params[:message]
 	user = User.get(session[:user_id])
 	Peep.create(:user => user, :message => peep)
 	redirect to('/')
+end
+
+get '/new' do
+	erb :new
 end
